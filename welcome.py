b340e032e341966b2edb81cd4e464208edf9596e
Thejshri <a.thejshri@gmail.com>
2024-05-22 21:24:30 +0530

    Welcome Message

---
+++ welcome.py
@@ -0,0 +1,12 @@
+line01 = "********************"  # header/footer
+line02 = "*                  *"  # Re-use
+line03 = "*     WELCOME!     *"
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+# Go to settings CNTRL+, => Format on save, autopep8
