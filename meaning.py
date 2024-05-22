0bce9fb3c0677e50d4434e692964c7fd7f25bc28
Thejshri <a.thejshri@gmail.com>
2024-05-22 21:50:20 +0530

    Meaning - Ternary

---
+++ meaning.py
@@ -0,0 +1,12 @@
+meaning = 32
+
+print("")
+
+if (meaning > 10):
+    print("That is greater than 10")
+else:
+    print("That is lesser than 10")
+
+# Ternary statement
+
+print("Greater") if meaning > 10 else print("Lesser")
